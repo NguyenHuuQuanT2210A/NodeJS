ec0fd0cc22747bde8d0c26aeb3f64541768d2a56
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2023-12-25 14:07:59 +0700

    fix lab01 & lab02

--- ch03-lab01/lab01.js
+++ ch03-lab01/lab01.js
@@ -7,22 +7,20 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
-app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => res.render('about'))
+app.get('/', (req, res) => res.render('home', {title: 'Home Page', message: 'Welcome to our website'}))
+app.get('/about', (req, res) => res.render('about', {title: 'About Us', message: 'Learn more about our company.'}))
 
 const port = process.env.PORT || 3000
 
 
 //custom 404 page
 app.use((req, res) => {
-    res.status(404)
-    res.render('404')
+    res.status(404).render('404', {title: '404 Not Found'})
 })
 
 //custom 500 page
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
+    console.error(err.stack);
+    res.status(500).render('500' ,{title: '500 Interal Server Error'});
 })
 app.listen(port, () => console.log(`server started on port ${port}; ` +  'press Ctrl-C to terminate.....'))
