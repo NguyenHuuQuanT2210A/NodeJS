259aeaa59bcecd5ef68f376ea2729429785f73d5
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2023-12-25 14:06:56 +0700

    fix lab01 & lab02

--- ch03-lab02/lab02.js
+++ ch03-lab02/lab02.js
@@ -5,33 +5,33 @@ const app = express()
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
 }))
-// extname: '.hbs' : đổi đuôi handlebars
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => res.render('about'))
 
 const products = [
     { name: 'Product 1', image: '/img/dua-hau.png' },
     { name: 'Product 2', image: '/img/qua-cam.jpg' }
 ]
 
+app.get('/', (req, res) => res.render('home', {title: 'Home Page', message: 'Welcome to our website'}))
+app.get('/about', (req, res) => res.render('about', {title: 'About Us', message: 'Learn more about our company.'}))
+
+
 app.get('/product', (req, res) => {
-    res.render('product',{ product: products })
+    res.render('product',{ products })
 })
+
+const port = process.env.PORT || 3000
+
 
 //custom 404 page
 app.use((req, res) => {
-    res.status(404)
-    res.render('404')
+    res.status(404).render('404', {title: '404 Not Found'})
 })
 
 //custom 500 page
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
+    console.error(err.stack);
+    res.status(500).render('500' ,{title: '500 Interal Server Error'});
 })
 app.listen(port, () => console.log(`server started on port ${port}; ` +  'press Ctrl-C to terminate.....'))
